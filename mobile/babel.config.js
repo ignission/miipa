6c0d103e0d26c79f561b87e8a32b3563c4d1cbdc
Shoma Nishitateno <shoma416@gmail.com>
2026-02-19 16:09:28 +0900

    Fix TestFlight crash: remove NativeWind v3 remnant and add gesture handler
    - Remove "nativewind/babel" preset (NativeWind v3 remnant causing module resolution errors in production)
    - Add react-native-gesture-handler as required peer dependency for expo-router
    - Wrap root layout with GestureHandlerRootView for gesture-based navigation
    - Remove unnecessary "use client" directive from React Native config

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -3,7 +3,6 @@ module.exports = (api) => {
 	return {
 		presets: [
 			["babel-preset-expo", { jsxImportSource: "nativewind" }],
-			"nativewind/babel",
 		],
 		plugins: ["react-native-reanimated/plugin"],
 	};
